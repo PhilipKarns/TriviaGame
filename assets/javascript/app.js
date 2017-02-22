1c13e1cf7758ffe842f365a9775e65de978f0e5a
Karns <pkarns@deloitte.com>
2017-02-22 15:51:08 -0500

    added countdown and quiz info. still need results page info

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,51 @@
-alert("hi");
-
+//hide form and click start button to begin the game
 window.onload = function() {
-	$("#start").on("click", countdown.start);
+	$("#content").hide();
+	$("#start").on("click", countdown.start);	
 }
 
+var intervalId;
+//create countdown object
 var countdown = {
 
+	time: 10,
+	
 	start: function() {
-		$('#start').hide();
+	//hide the start button and show form. Use setInterval to start the count.
+		$("#start").hide();
+		$("#content").show();
 		intervalId = setInterval(countdown.count, 1000);
 	},
 	count: function() {
+		//decrement the time by 1
 		countdown.time--;
+		//pass the current time into the timeConverter function
+		var converted = countdown.timeConverter(countdown.time);
+		//show converted time in display div
+		$("#display").html(converted);
 	},
+	timeConverter: function(t) {
+		var minutes = Math.floor(t / 60);
+    	var seconds = t - (minutes * 60);
+
+    	if (seconds < 10) {
+    		seconds = "0" + seconds;
+    	}
+    	 if (seconds == 0) {
+			countdown.stop(seconds);
+
+    	 	
+    	 }    	
+    	return minutes + ":" + seconds;
+    }, //timeConverter end bracket
+    
+    //stop at 0:00
+    stop: function(stop) {
+    	 var stop = clearInterval(intervalId);
+    	 alert('You Lose');
+    	 $("#content").hide();
+    	 $("#results").show();
+    },
+
+
 };
